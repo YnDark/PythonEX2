f96f9b7a6849ae9d7acd79e5fb84f771a86eb7fc
Ynmio <1920224395@qq.com>
2024-06-11 22:56:25 +0800

    除了饼图都已完成,完善了实验

--- 202231061233_杨定鑫.py
+++ 202231061233_杨定鑫.py
@@ -13,21 +13,19 @@ df = pd.read_csv(r"D:\杨定鑫\Documents\py实验2\实验2\实验2\ETTh1.csv")
 print(df)
 
 # 计算每周油温的均值，绘制折线图和散点图。
-
-# 计算每周油温的均值
+## 计算每周油温的均值
 df['date'] = pd.to_datetime(df['date'])
 df['year_week'] = df['date'].dt.strftime('%Y-%V')
-df.to_csv("allData.csv")
-
-## 计算每周油温的均值
 OT_Mean = df.groupby(['year_week']).mean().reset_index()
-print(OT_Mean)
+print(OT_Mean[['year_week', 'OT']])
 
 # 绘制折线图
 ## 设置画布大小
-fig = plt.figure(figsize=(10, 9))
+fig = plt.figure(figsize=(11, 9))
+plt.plot(OT_Mean['year_week'], OT_Mean['OT'], color='black')
 
-plt.plot(OT_Mean['year_week'], OT_Mean['OT'])
+## 设置图例
+plt.legend(("平均油温",))
 
 ## 设置刻度和x标签
 plt.xticks(ticks=range(0, len(OT_Mean['year_week']), 5), labels=OT_Mean['year_week'][::5], rotation=90)
@@ -43,15 +41,18 @@ plt.show()
 
 # 绘制散点图
 ## 设置画布大小
-fig = plt.figure(figsize=(10, 9))
+fig = plt.figure(figsize=(11, 9))
 
 plt.plot(OT_Mean['year_week'], OT_Mean['OT'], 'ro', mfc='w')
 
 ## 设置刻度和x标签
 plt.xticks(ticks=range(0, len(OT_Mean['year_week']), 5), labels=OT_Mean['year_week'][::5], rotation=90)
 
+## 设置图例
+plt.legend(("平均油温",), loc='best')
+
 ## 设置标题
-plt.title('平均油温图', fontsize=20, loc='center', pad=1.5)
+plt.title('平均油温图', fontsize=20, loc='center')
 
 ## 设置坐标轴标题
 plt.xlabel("年份-第几周", fontsize=13, loc='center', labelpad=15)
@@ -63,11 +64,12 @@ plt.show()
 df['year'] = df['date'].dt.strftime('%Y')
 print(df)
 
-# 按年统计均值、最大值、最小值
+## 按年统计均值、最大值、最小值
 data = df.groupby(['year']).agg(
-    {'HUFL': {'mean', 'max'}, 'MUFL': {'mean', 'max'}, 'LUFL': {'mean', 'max'}}).reset_index()
+    {'HUFL': {'mean', 'min', 'max'}, 'MUFL': {'mean', 'min', 'max'}, 'LUFL': {'mean', 'min', 'max'}}).reset_index()
+print(data)
 
-# 使用多柱状图展示
+## 使用多柱状图展示
 ## 获取数据
 year = data['year']
 intYear = year.astype(int)
@@ -82,9 +84,9 @@ fig = plt.figure(figsize=(12, 9))
 
 ## 平均值绘制柱状图
 width = round(1.0 / (len(intYear) * 2), 1)
-plt.bar(intYear + width, HUFL['mean'], width, label='HUFL')
-plt.bar(intYear, MUFL['mean'], width, label='MUFL')
-plt.bar(intYear - width, LUFL['mean'], width, label='LUFL')
+plt.bar(intYear + width, HUFL['mean'], width, label='HUFL', color='orange')
+plt.bar(intYear, MUFL['mean'], width, label='MUFL', color='blue')
+plt.bar(intYear - width, LUFL['mean'], width, label='LUFL', color='green')
 
 ## 设置x轴刻度
 plt.xticks(ticks=range(intYear[0] + 0, intYear[0] + len(intYear)), labels=year + "年")
@@ -97,7 +99,7 @@ plt.ylabel('平均值', fontsize=13, loc='center', labelpad=15)
 plt.title('均值图像', fontsize=20, loc='center', pad=1.5)
 
 ## 设置图例
-plt.legend(['HUFL', 'MUFL', 'LUFL'], loc='upper right')
+plt.legend(['HUFL', 'MUFL', 'LUFL'], loc='best')
 
 ## 设置柱状图标识
 for a, b in zip(intYear + width, HUFL['mean']):
@@ -116,9 +118,9 @@ plt.show()
 fig = plt.figure(figsize=(12, 9))
 
 ## 最大值绘制柱状图
-plt.bar(intYear + width, HUFL['max'], width, label='HUFL')
-plt.bar(intYear, MUFL['max'], width, label='MUFL')
-plt.bar(intYear - width, LUFL['max'], width, label='LUFL')
+plt.bar(intYear + width, HUFL['max'], width, label='HUFL', color='orange')
+plt.bar(intYear, MUFL['max'], width, label='MUFL', color='blue')
+plt.bar(intYear - width, LUFL['max'], width, label='LUFL', color='green')
 
 ## 设置x轴刻度
 plt.xticks(ticks=range(intYear[0] + 0, intYear[0] + len(intYear)), labels=year + "年")
@@ -131,7 +133,7 @@ plt.ylabel('最大值', fontsize=13, loc='center', labelpad=15)
 plt.title('最大值图像', fontsize=20, loc='center', pad=1.5)
 
 ## 设置图例
-plt.legend(['HUFL', 'MUFL', 'LUFL'], loc='upper right')
+plt.legend(['HUFL', 'MUFL', 'LUFL'], loc='best')
 
 ## 设置柱状图标识
 for a, b in zip(intYear + width, HUFL['max']):
@@ -145,6 +147,39 @@ for a, b in zip(intYear - width, LUFL['max']):
 
 plt.show()
 
+## 设置画布大小
+fig = plt.figure(figsize=(12, 9))
+
+## 最小值绘制柱状图
+plt.bar(intYear + width, HUFL['min'], width, label='HUFL', color='orange')
+plt.bar(intYear, MUFL['min'], width, label='MUFL', color='blue')
+plt.bar(intYear - width, LUFL['min'], width, label='LUFL', color='green')
+
+## 设置x轴刻度
+plt.xticks(ticks=range(intYear[0] + 0, intYear[0] + len(intYear)), labels=year + "年")
+
+## 设置轴标签
+plt.xlabel('年份', fontsize=13, loc='center', labelpad=15)
+plt.ylabel('最小值', fontsize=13, loc='center', labelpad=15)
+
+## 设置标题
+plt.title('最小值图像', fontsize=20, loc='center', pad=1.5)
+
+## 设置图例
+plt.legend(['HUFL', 'MUFL', 'LUFL'], loc='best')
+
+## 设置柱状图标识
+for a, b in zip(intYear + width, HUFL['min']):
+    plt.text(a, b, format(round(b, 1), ','), ha='center', va='bottom', fontsize=11)
+
+for a, b in zip(intYear, MUFL['min']):
+    plt.text(a, b, format(round(b, 1), ','), ha='center', va='bottom', fontsize=11)
+
+for a, b in zip(intYear - width, LUFL['min']):
+    plt.text(a, b, format(round(b, 1), ','), ha='center', va='bottom', fontsize=11)
+
+plt.show()
+
 # 对油温数据，绘制直方图。自己按照最大、最小值划分区间。
 ## 计算区间个数
 OT_max = df['OT'].max()
@@ -153,8 +188,17 @@ bins = int(OT_max - OT_min)
 print(OT_max)
 print(OT_min)
 
+## 设置画布大小
+fig = plt.figure(figsize=(12, 9))
+
 ## 绘图
-plt.hist(df['OT'], bins=bins)
+plt.hist(df['OT'], bins=bins, color='gray')
+
+## 设置标题
+plt.title("油温直方图")
+
+## 设置图例
+plt.legend(("油温出现次数",))
 
 ## 设置标签
 plt.xlabel('油温')
@@ -171,12 +215,25 @@ plt.show()
 HULL = df['HULL']
 MUFL = df['MUFL']
 LULL = df['LULL']
+
 plt.boxplot([HULL, MUFL, LULL],
             whis=1.5,
+            labels=['HULL', 'MUFL', 'LULL'],
             showmeans=True
             , flierprops={'markerfacecolor': 'red', 'markeredgecolor': 'red', 'markersize': 3}
             , meanprops={'markerfacecolor': 'black', 'markeredgecolor': 'black', 'markersize': 8, 'marker': 'h'}
             , medianprops={'linestyle': '--', 'color': 'orange'})
+
+## 设置标题
+plt.title("高无效负载、中间无效负载、低无效负载箱型图")
+
+## 设置图例
+plt.legend(["上下限", "平均值", "上下限"])
+
+## 设置坐标轴标签
+plt.xlabel("数据类型")
+plt.ylabel("对应数据大小")
+
 plt.show()
 # 计算上四分位数和下四分位数
 Q1_HULL = HULL.quantile(0.25)
@@ -214,12 +271,24 @@ print("val_LULL-----------------------------")
 print(val_LULL)
 
 # 从高有效负载数据这列中，选择任意连续7天的数据，绘制热力图。
-HUFL = df['HUFL'][1000:1006]
-print(HUFL)
+df[['date', 'HUFL']].map( )
+
+## 绘制热力图
 plt.imshow([HUFL])
+
+## 显示颜色条
 plt.colorbar()
+
+## 设置两轴刻度
 plt.xticks(range(6), df['date'][1000:1006], rotation=90)
 plt.yticks([0], ["HUFL"])
+
+## 设置标题
+plt.title("HUFL其中连续七天数据热力图")
+
+## 设置两轴标签
+plt.xlabel("时间")
+
 plt.show()
 
 # 选取高无效负载、中间无效负载和低无效负载三列数据，绘制2行2列的多子图
@@ -236,18 +305,17 @@ fig = plt.figure(figsize=(20, 11))
 plt.subplot(2, 2, 1)
 plt.plot(date, HUFL)
 #plt.xticks(rotation=90)
-plt.xlabel("月份-日期 时间",fontsize=13)
-plt.ylabel("HUFL",fontsize=13)
+plt.xlabel("月份-日期 时间", fontsize=13)
+plt.ylabel("HUFL", fontsize=13)
 
 plt.subplot(2, 2, 2)
-plt.hist(MUFL,bins=20)
-plt.xlabel("MUFL",fontsize=13)
-plt.ylabel("出现次数",fontsize=13)
+plt.hist(MUFL, bins=20)
+plt.xlabel("MUFL", fontsize=13)
+plt.ylabel("出现次数", fontsize=13)
 
 plt.subplot(2, 1, 2)
 plt.plot(date, LUFL, 'ro')
-plt.xlabel("月份-日期 时间",fontsize=13)
-plt.ylabel("LUFL",fontsize=13)
-
+plt.xlabel("月份-日期 时间", fontsize=13)
+plt.ylabel("LUFL", fontsize=13)
 
 plt.show()
